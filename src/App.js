eedc388ebd2965a2d451222e299c5245d0f9926d
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-02-12 05:28:30 +0600

    make components functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import CreateArea from "./CreateArea";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -25,7 +25,17 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
