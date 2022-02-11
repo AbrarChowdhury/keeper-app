36ab4fca58f7745c497eb2cf108ea6e719d08183
abrar shams chowdhury <abrar.chowdhury99@gmail.com>
2022-02-12 05:06:40 +0600

    set up all components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+
+      <Footer />
     </div>
   );
 }
